4087cf079f6c7ad4210a7b58eba7b1edef5c10d9
leviobrabo <carlosjunior20313@gmail.com>
2023-05-09 09:55:17 -0300

    update bot alteração

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -627,7 +627,7 @@ async function sendHistoricalEventsChannel(channelId) {
 }
 
 const channelJob = new CronJob(
-    "53 8 * * *",
+    "57 8 * * *",
     function () {
         sendHistoricalEventsChannel(channelId);
         console.log(`Message successfully sent to the channel ${channelId}`);
